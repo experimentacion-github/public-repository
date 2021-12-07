5ac38ba0a13be28ae71f97ca4964637e4aef3f13
pablo <gutiruizp@gmail.com>
2021-12-07 13:32:15 +0100

    Create codeql-analysis.yml (#4)
    * Create codeql-analysis.yml
    
    * Add high complexity function
    
    * Add extra blank line
    
    * Remove extra blank line
    
    Co-authored-by: pablo.gutierrez <pablo.gutierrez@iic.uam.es>

--- samplepackage/basic_functions.py
+++ samplepackage/basic_functions.py
@@ -11,6 +11,71 @@ def make_me_feel_very_bad():
     return "Tomorrow is Monday... :("
 
 
+def high_complexity():
+    i = 0
+    for i in range(5):
+        for j in range(5):
+            for m in range(5):
+                for p in range(5):
+                    for q in range(5):
+                        i += 1
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    if i > 3:
+        if j > 3:
+            if m > 3:
+                for p in range(5):
+                    for p in range(5):
+                        for p in range(5):
+                            if q > 3:
+                                i = 0
+
+    return True
+
+
 def day_evaluator(day):
     if day in ["Monday", "Tuesday"]:
         return 2
